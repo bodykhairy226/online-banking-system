5ca90861a1ac20ce0e05094edb58f4a4d42a22a5
bodykhairy226 <bodykhairy226@gmail.com>
2026-03-12 01:20:50 +0200

    fix mongodb connection

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const SALT_ROUNDS = 10;
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log("Connected to MongoDB Atlas"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+
 
 // Middlewares
 app.use(
