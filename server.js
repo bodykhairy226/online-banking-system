1312b040a2f937818712e1c8f5a013b14a14fad6
bodykhairy226 <bodykhairy226@gmail.com>
2026-04-03 19:32:19 +0200

    fix mongo env variable

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
     // server.js
+    console.log("DB NAME:", mongoose.connection.name);
+console.log("DB HOST:", mongoose.connection.host);
     import dotenv from "dotenv";
 dotenv.config();
     import helmet from "helmet";
@@ -29,8 +31,7 @@ const limiter = rateLimit({
 
 mongoose.set("strictQuery", true);
 console.log("ENV TEST:", process.env.MONGO);
-console.log("DB NAME:", mongoose.connection.name);
-console.log("DB HOST:", mongoose.connection.host);
+
 mongoose.connect(process.env.MONGO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
