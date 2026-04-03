7826814e5f8ea24aee4a6a31631eef1431d84aa1
bodykhairy226 <bodykhairy226@gmail.com>
2026-04-03 18:09:40 +0200

    fix mongo env variable

--- server.js
+++ server.js
@@ -28,13 +28,19 @@ const limiter = rateLimit({
     app.disable("x-powered-by");
 
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.set("strictQuery", true);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => {
   console.log("MongoDB Connected ✅");
 })
 .catch((err) => {
   console.error("MongoDB Error ❌:", err);
 });
+
      
     // Middlewares
   
